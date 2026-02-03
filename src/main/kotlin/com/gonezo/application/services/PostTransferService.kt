73cae7b80e254a248ea511e605594405607a9032
BrunoPicolo <bruno.picolo.ortiz@gmail.com>
2026-02-03 18:53:50 +0000

    feat: add post transfer transaction use case

---
+++ src/main/kotlin/com/gonezo/application/services/PostTransferService.kt
@@ -0,0 +1,30 @@
+package com.gonezo.application.services
+
+import com.gonezo.application.PostTransferCommand
+import com.gonezo.application.PostTransferUC
+import com.gonezo.domain.cashledger.ports.TransactionRepository
+import com.gonezo.domain.cashledger.services.LedgerPostingService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Service
+class PostTransferService(
+  private val ledgerPostingService: LedgerPostingService,
+  private val transactionRepository: TransactionRepository,
+) : PostTransferUC {
+
+  @Transactional
+  override fun execute(command: PostTransferCommand): List<UUID> {
+    val transactions = ledgerPostingService.postTransfer(
+      fromAccountId = command.fromAccountId,
+      toAccountId = command.toAccountId,
+      postedDate = command.postedDate,
+      effectiveDate = command.effectiveDate,
+      amount = command.amount,
+    )
+
+    transactions.forEach { transactionRepository.save(it) }
+    return transactions.map { it.id }
+  }
+}
