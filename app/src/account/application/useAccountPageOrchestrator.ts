aafad96f1c8c5979c01e520736c95eb64709f11e
BrunoPicolo <bruno.picolo.ortiz@gmail.com>
2026-03-24 13:11:07 +0000

    refactor(account): route accounts page through account module

---
+++ app/src/account/application/useAccountPageOrchestrator.ts
@@ -0,0 +1,2 @@
+export type { AccountsCorePort } from '../../app/accounts/useAccountsPageModel';
+export { useAccountsPageModel as useAccountPageOrchestrator } from '../../app/accounts/useAccountsPageModel';
