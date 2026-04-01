7de55acb490b16dac512155d7d0e75c145455aa8
BrunoPicolo <bruno.picolo.ortiz@gmail.com>
2026-04-01 13:16:11 +0100

    refactor(android): remove legacy voice draft processor

--- app/android/app/src/main/java/com/gonezo/multiplatform/plugins/voice/VoiceDraftProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.gonezo.multiplatform.plugins.voice;
-
-import com.getcapacitor.JSObject;
-
-public interface VoiceDraftProcessor {
-  JSObject process(VoiceDraftProcessingInput input);
-}
