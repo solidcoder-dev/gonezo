fa6dbac067ac6875d0e7a865778b03bc02553b73
bruno.picolo <bruno.picolo.ortiz@gmail.com>
2026-07-27 16:03:26 +0100

    integrate bootstrap palette

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 
+import './styles/bootstrap.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
 import './shared/ui/primitives.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
